456babc0e7fa95cf5179303760d1276ef91b8a32
Naufal Gajendra Ardiputra <53498520+naufal025@users.noreply.github.com>
2023-10-10 15:08:39 +0700

    Add files via upload

---
+++ jobsheet7/DoWhileCuti22_1.java
@@ -0,0 +1,33 @@
+package jobsheet7;
+import java.util.Scanner;
+public class DoWhileCuti22_1 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int jatahCuti, jumlahHari;
+        String konfirmasi;
+
+        System.out.print("Jatah Cuti: ");
+        jatahCuti = scan.nextInt();
+
+        do {
+            System.out.print("Apakah anda ingin mengambil cuti (y/t)? ");
+            konfirmasi = scan.next();
+
+            if(konfirmasi.equalsIgnoreCase("y")) {
+                System.out.print("Jumlah hari: ");
+                jumlahHari = scan.nextInt();
+
+                if (jumlahHari <= jatahCuti) {
+                    jatahCuti -= jumlahHari;
+                    System.out.println("Sisa jatah cuti: " + jatahCuti);
+                } else if (jatahCuti == 0) {
+                    break;
+                } else{
+                    System.out.println("Sisa jatah cuti anda adalah " +jatahCuti+ " Harap isi dengan jatah cuti anda");
+                }
+            }
+        } while (jatahCuti > 0);
+    }
+}
+    
+
