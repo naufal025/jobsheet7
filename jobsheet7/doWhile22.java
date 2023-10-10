7d0b4ab27e0e443d7b3372f74d12ab599f01666e
Naufal Gajendra Ardiputra <53498520+naufal025@users.noreply.github.com>
2023-10-10 15:43:43 +0700

    Add files via upload

---
+++ jobsheet7/doWhile22.java
@@ -0,0 +1,18 @@
+package jobsheet7;
+
+public class doWhile22 {
+    public static void main(String[] args) {
+        int x = 0;
+        do {
+            System.out.println(x);
+        } while (++x <= 8);
+        System.out.println("Program berhenti");
+
+        //do while dengan nilai x = 10
+         //int x = 10;
+        //do {
+            //System.out.println(x);
+        //} while (++x <= 8);
+        //System.out.println("Program berhenti");
+    }
+}
